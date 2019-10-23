624c98b839ae54ab3db44c2abcde232c770f17c9
Collin Messer <collin@collinmesser.com>
2019-10-23 06:54:08 -0400

    :rocket: Issue 75 (#98)
    * Add vuex persisted state, add authorized state, fix a few bugs
    
    * Clean up eslint errors
    
    * Fix firebase.auth() bug when click event is in form element
    
    * Cleanup messaging and layout a little and remove console logs

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -23,14 +23,31 @@ export default {
   padding: 15px;
   color: #FFFFFF;
   font-size: 14px;
-  -webkit-transition: all 0.3 ease;
-  transition: all 0.3 ease;
   cursor: pointer;
+  white-space: nowrap;
 }
 
 button:hover,button:active,button:focus {
   background: #43A047;
 }
+
+.header__user .button {
+  padding: 0 20px;
+}
+
+.header__user button:hover,button:active,button:focus {
+  background: #43A047;
+}
+
+/* .button__social .button {
+  width: 75px;
+  background: #fff;
+  padding: 10px;
+  border-radius: 100%;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  outline: 0;
+  border: 0;
+} */
 
 .button-light {
 
--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="profile">Profile</div>
+  <div class="profile">
+    <h1>Profile</h1>
+    <a href="#" @click.prevent.stop="logout">Logout</a>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase';
+import store from '@/store.js'; // eslint-disable-line
+
 export default {
   name: 'Profile',
+  methods: {
+    logout() {
+      firebase.auth().signOut().then(() => {
+        store.commit('setAuthorization', false);
+        this.$router.replace('home');
+      });
+    },
+  },
 };
 </script>
 
